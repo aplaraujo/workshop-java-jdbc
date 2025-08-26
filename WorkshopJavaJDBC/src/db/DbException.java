bffbd9a8b42ded8852fe4a030c3257f48c3c96b9
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-26 17:22:07 -0300

    Project creation with Maven

--- WorkshopJavaJDBC/src/db/DbException.java
+++
@@ -1,9 +0,0 @@
-package db;
-
-public class DbException extends RuntimeException { // Tratamento de erros
-    private static final long serialVersionUID = 1L;
-
-    public DbException(String message) {
-        super(message);
-    }
-}
