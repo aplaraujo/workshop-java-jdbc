42d482d0ed2b27479b6e1345b1a23cac5f42db23
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-26 18:32:01 -0300

    Creation order class

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -6,17 +6,29 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.example.db.DB;
+import com.example.entities.Product;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        Connection conn = DB.getConnection();
-	
+	public static void main(String[] args) throws SQLException {
+		Connection conn = DB.getConnection();
+
 		Statement st = conn.createStatement();
-			
+
 		ResultSet rs = st.executeQuery("select * from tb_product");
-			
+
 		while (rs.next()) {
-			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+			Product prod = instantiateProduct(rs);
+			System.out.println(prod);
 		}
-    }
+	}
+
+	private static Product instantiateProduct(ResultSet rs) throws SQLException {
+		Product prod = new Product();
+		prod.setId(rs.getLong("id"));
+		prod.setDescription(rs.getString("description"));
+		prod.setName(rs.getString("name"));
+		prod.setImageUri(rs.getString("image_uri"));
+		prod.setPrice(rs.getDouble("price"));
+		return prod;
+	}
 }
